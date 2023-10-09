311dc7e1a7ffb3aac83d7301738d12b29f9a3807
mrvillage <josef@mrvillage.dev>
2023-10-09 11:03:52 -0400

    Add full documentation and serde support

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,7 @@ use rand_core::{OsRng, RngCore};
 use thiserror::Error;
 use totp_rs::{Rfc6238, Rfc6238Error, Secret, SecretParseError, TotpUrlError, TOTP};
 
+/// The general error type for this crate
 #[derive(Error, Debug)]
 pub enum AuthError<B>
 where
@@ -90,6 +91,7 @@ where
     }
 }
 
+/// Errors associated with using TOTP (two factor authentication)
 #[derive(Error, Debug)]
 pub enum TotpError {
     #[error(transparent)]
@@ -154,30 +156,52 @@ where
     }
 }
 
+/// The type of token issued
 #[derive(Debug)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub enum TokenType {
+    /// A token that can be used to authenticate the user
     Auth,
+    /// A token that can be used to complete the login process alongside a 2FA code
     TwoFactorRequired,
+    /// A single token that can be used to complete the login process and get an auth token
     Magic,
 }
 
+/// A general type for user IDs, allows the backend to use either a numeric or text ID as the
+/// implementer sees fit
 #[derive(Debug)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub enum Id {
     Numeric(u64),
     Text(String),
 }
 
+/// Represents a full token
 #[derive(Debug)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct Token {
+    /// The ID of the user this token is for
     pub user_id: Id,
+    /// The token itself
     pub token: String,
+    /// The time this token was issued
     pub issued: DateTime<Utc>,
+    /// The time this token expires
     pub expiry: DateTime<Utc>,
+    /// The type of token
     pub r#type: TokenType,
+    /// Whether this token has been revoked
     pub revoked: bool,
 }
 
 impl Token {
+    /// Create a new token
+    ///
+    /// # Arguments
+    /// * `user_id` - The ID of the user this token is for
+    /// * `valid_for` - How long this token is valid for
+    /// * `type` - The type of token
     pub fn new(
         user_id: impl Into<Id>,
         valid_for: impl Into<Duration>,
@@ -196,6 +220,13 @@ impl Token {
         }
     }
 
+    /// Create a new token from a backend and context, taking the user ID from the context and the
+    /// token expiry from the backend
+    ///
+    /// # Arguments
+    /// * `backend` - The backend to use
+    /// * `ctx` - The context to use
+    /// * `type` - The type of token
     pub fn from_ctx<B>(backend: &B, ctx: &B::Context, r#type: impl Into<TokenType>) -> Self
     where
         B: Backend + ?Sized,
@@ -208,6 +239,11 @@ impl Token {
         )
     }
 
+    /// Check if this token is valid, returns `Ok(())` if it is valid, otherwise returns an error
+    ///
+    /// # Errors
+    /// * `AuthError::TokenRevoked` - The token has been revoked
+    /// * `AuthError::TokenExpired` - The token has expired
     pub fn check_validity<B>(&self) -> Result<(), AuthError<B>>
     where
         B: Backend + ?Sized,
@@ -222,9 +258,15 @@ impl Token {
     }
 }
 
+/// Represents an OAuth2 session
+/// This is used to start the OAuth2 flow
+/// The `auth_url` field should be used to redirect the user to the OAuth2 provider
+/// The remainder of this session should be stored in the backend and used to complete the
+/// authentication when the user is redirected back
 #[derive(Debug)]
 pub struct OAuthSession {
     client: BasicClient,
+    /// The authentication URL to redirect the user to
     pub auth_url: Url,
     csrf_token: CsrfToken,
     pkce_verifier: PkceCodeVerifier,
@@ -232,29 +274,56 @@ pub struct OAuthSession {
 
 #[derive(Debug)]
 pub struct OAuthResult {
+    /// The authentication token that was generated
     pub token: Token,
+    /// The OAuth2 access token
     pub access_token: String,
+    /// The OAuth2 refresh token
     pub refresh_token: Option<String>,
+    /// The OAuth2 access token expiry
     pub expires_in: Option<std::time::Duration>,
+    /// The OAuth2 scopes that were granted
     pub scopes: Option<Vec<String>>,
 }
 
+/// The backend trait, implement this for your backend
 #[async_trait]
 pub trait Backend {
+    /// The context type, this is used to store any backend specific data that is required
+    /// Generally, user data such as the email, 2FA token, password hash, etc will be stored in
+    /// here and this trait will syncronously ask the implementer for this data with the provided
+    /// context when required.
     type Context: Send + Sync;
+    /// The error type, this is used to store any backend specific errors that may occur
     type Error: std::error::Error + std::fmt::Display;
+    /// The issuer to use for TOTP
     const TOTP_ISSUER: &'static str;
 
+    /// Hash a password
     #[inline]
     fn hash_password(&self, password: &str) -> String {
         password_auth::generate_hash(password.as_bytes())
     }
 
+    /// Verify a password
     #[inline]
     fn verify_password(&self, input: &str, hash: &str) -> Result<(), password_auth::VerifyError> {
         password_auth::verify_password(input, hash)
     }
 
+    /// Login with a password
+    /// This will hash the password and verify it against the stored hash
+    /// If the password is correct, a token will be generated and returned
+    /// If two factor authentication is enabled, a token of type `TwoFactorRequired` will be
+    ///
+    /// # Arguments
+    /// * `ctx` - The context to use
+    /// * `input` - The password to verify
+    ///
+    /// # Errors
+    /// * `AuthError::PasswordParseError` - The stored password hash is invalid
+    /// * `AuthError::PasswordInvalid` - The password is incorrect
+    /// * `AuthError::BackendError` - A backend error occurred while generating the token
     async fn login_with_password(
         &self,
         ctx: &Self::Context,
@@ -277,6 +346,15 @@ pub trait Backend {
         .await
     }
 
+    /// Get the duration a token of the given type is valid for
+    /// This is used to determine the expiry of the token
+    /// The default implementation returns 15 minutes for `Magic` tokens, 5 minutes for
+    /// `TwoFactorRequired` tokens and the value returned by `get_auth_token_valid_duration` for
+    /// `Auth` tokens
+    ///
+    /// # Arguments
+    /// * `ctx` - The context to use
+    /// * `type` - The type of token
     #[inline]
     fn get_token_valid_duration(&self, ctx: &Self::Context, r#type: &TokenType) -> Duration {
         match r#type {
@@ -286,26 +364,84 @@ pub trait Backend {
         }
     }
 
+    /// Get the password hash for the given context
+    ///
+    /// # Arguments
+    /// * `ctx` - The context to use
     fn get_password_hash<'c>(&self, ctx: &'c Self::Context) -> &'c str;
+    /// Get whether two factor authentication is enabled for the given context
+    ///
+    /// # Arguments
+    /// * `ctx` - The context to use
     fn is_two_factor_enabled(&self, ctx: &Self::Context) -> bool;
+    /// Get the duration an auth token is valid for
+    ///
+    /// # Arguments
+    /// * `ctx` - The context to use
     fn get_auth_token_valid_duration(&self, ctx: &Self::Context) -> Duration;
+    /// Get the user ID for the given context
+    ///
+    /// # Arguments
+    /// * `ctx` - The context to use
     fn get_user_id(&self, ctx: &Self::Context) -> Id;
+    /// Save the given token to the backend
+    ///
+    /// # Arguments
+    /// * `ctx` - The context to use
+    /// * `token` - The token to save
     async fn save_token(&self, ctx: &Self::Context, token: &Token) -> Result<(), Self::Error>;
+    /// Get the token with the given token string
+    ///
+    /// # Arguments
+    /// * `ctx` - The context to use
+    /// * `token` - The token string to get
     async fn get_token(&self, ctx: &Self::Context, token: &str) -> Result<Token, Self::Error>;
+    /// Revoke the given token
+    ///
+    /// # Arguments
+    /// * `ctx` - The context to use
+    /// * `token` - The token to revoke
     async fn revoke_token(&self, ctx: &Self::Context, token: &Token) -> Result<(), Self::Error>;
+    /// Store that the given TOTP code has been used
+    ///
+    /// # Arguments
+    /// * `ctx` - The context to use
+    /// * `code` - The code to store
     async fn store_totp_code_used(
         &self,
         ctx: &Self::Context,
         code: &str,
     ) -> Result<(), Self::Error>;
+    /// Get the TOTP secret for the given context
+    ///
+    /// # Arguments
+    /// * `ctx` - The context to use
     fn get_totp_secret<'c>(&self, ctx: &'c Self::Context) -> &'c str;
+    /// Get the TOTP account name for the given context
+    ///
+    /// # Arguments
+    /// * `ctx` - The context to use
     fn get_totp_account_name(&self, ctx: &Self::Context) -> String;
+    /// Check if the given TOTP code has been used
+    ///
+    /// # Arguments
+    /// * `ctx` - The context to use
+    /// * `code` - The code to check
     async fn has_totp_code_been_used(
         &self,
         ctx: &Self::Context,
         code: &str,
     ) -> Result<bool, Self::Error>;
 
+    /// Get a TOTP instance for the given context and secret
+    /// This will return an error if the secret is invalid
+    ///
+    /// # Arguments
+    /// * `ctx` - The context to use
+    /// * `secret` - The secret to use
+    ///
+    /// # Errors
+    /// * `AuthError::TotpLibError` - The secret is invalid
     #[inline]
     fn totp(&self, ctx: &Self::Context, secret: &str) -> Result<TOTP, AuthError<Self>> {
         Ok(TOTP::from_rfc6238(Rfc6238::new(
@@ -316,11 +452,29 @@ pub trait Backend {
         )?)?)
     }
 
+    /// Get a TOTP instance for the given context
+    ///
+    /// # Arguments
+    /// * `ctx` - The context to use
+    ///
+    /// # Errors
+    /// * `AuthError::TotpLibError` - The secret is invalid
     #[inline]
     fn totp_from_ctx(&self, ctx: &Self::Context) -> Result<TOTP, AuthError<Self>> {
         self.totp(ctx, self.get_totp_secret(ctx))
     }
 
+    /// Use the given TOTP code
+    /// This will check the code is valid, has not been used before and store that it has been used
+    /// If all of these checks pass, `Ok(true)` will be returned, otherwise `Ok(false)` will be
+    ///
+    /// # Arguments
+    /// * `ctx` - The context to use
+    /// * `code` - The code to use
+    ///
+    /// # Errors
+    /// * `AuthError::TotpLibError` - The secret is invalid
+    /// * `AuthError::BackendError` - A backend error occurred while checking the code
     async fn use_totp_code(
         &self,
         ctx: &Self::Context,
@@ -338,6 +492,23 @@ pub trait Backend {
                 .map(|()| true)?)
     }
 
+    /// Submit a TOTP code
+    /// This will check the code is valid and has not been used before
+    /// If all of these checks pass, a authentication token will be generated and returned
+    /// If the checks fail, an error will be returned
+    /// If the token is generated, the old token will be revoked
+    /// This will return an error if the token is not of type `TwoFactorRequired`
+    ///
+    /// # Arguments
+    /// * `ctx` - The context to use
+    /// * `token` - The token to submit
+    /// * `code` - The code to use
+    ///
+    /// # Errors
+    /// * `AuthError::TotpLibError` - The secret is invalid
+    /// * `AuthError::BackendError` - A backend error occurred while checking the code
+    /// * `AuthError::TotpCodeInvalid` - The code is invalid
+    /// * `AuthError::IncorrectTokenType` - The token is not of type `TwoFactorRequired`
     async fn submit_totp(
         &self,
         ctx: &Self::Context,
@@ -363,21 +534,47 @@ pub trait Backend {
         }
     }
 
+    /// Get the current TOTP code
+    ///
+    /// # Arguments
+    /// * `ctx` - The context to use
+    ///
+    /// # Errors
+    /// * `AuthError::TotpLibError` - The secret is invalid
     #[inline]
     fn get_current_totp_code(&self, ctx: &Self::Context) -> Result<String, AuthError<Self>> {
         Ok(self.totp_from_ctx(ctx)?.generate_current()?)
     }
 
+    /// Generate a Base32 TOTP secret
     #[inline]
     fn generate_totp_secret_base_32(&self) -> String {
         Secret::generate_secret().to_encoded().to_string()
     }
 
+    /// Generate a magic token
+    /// This will generate a token of type `Magic` and save it to the backend
+    /// This token can be used to complete the login process
+    ///
+    /// # Arguments
+    /// * `ctx` - The context to use
+    ///
+    /// # Errors
+    /// * `AuthError::BackendError` - A backend error occurred while generating the token
     #[inline]
     async fn generate_magic_token(&self, ctx: &Self::Context) -> Result<Token, AuthError<Self>> {
         self.generate_token(ctx, TokenType::Magic).await
     }
 
+    /// Generate a token
+    /// This will generate a token of the given type and save it to the backend
+    ///
+    /// # Arguments
+    /// * `ctx` - The context to use
+    /// * `type` - The type of token to generate
+    ///
+    /// # Errors
+    /// * `AuthError::BackendError` - A backend error occurred while saving the token
     async fn generate_token(
         &self,
         ctx: &Self::Context,
@@ -390,6 +587,23 @@ pub trait Backend {
         Ok(token)
     }
 
+    /// Redeem a magic token
+    /// This will check the token is valid and has not been used before
+    /// If all of these checks pass, a authentication token will be generated and returned
+    /// If the checks fail, an error will be returned
+    /// If the token is generated, the old token will be revoked
+    /// This will return an error if the token is not of type `Magic`
+    ///
+    /// # Arguments
+    /// * `ctx` - The context to use
+    /// * `token` - The token to redeem
+    ///
+    /// # Errors
+    /// * `AuthError::BackendError` - A backend error occurred while checking the token
+    /// * `AuthError::TokenExpired` - The token has expired
+    /// * `AuthError::TokenRevoked` - The token has been revoked
+    /// * `AuthError::IncorrectTokenType` - The token is not of type `Magic`
+    /// * `AuthError::BackendError` - A backend error occurred while generating the token
     async fn redeem_magic_token(
         &self,
         ctx: &Self::Context,
@@ -410,11 +624,23 @@ pub trait Backend {
         }
     }
 
+    /// Get the TOTP URL for the given context
+    ///
+    /// # Arguments
+    /// * `ctx` - The context to use
     #[inline]
     fn get_totp_url(&self, ctx: &Self::Context) -> Result<String, AuthError<Self>> {
         Ok(self.totp_from_ctx(ctx)?.get_url())
     }
 
+    /// Get the TOTP QR code for the given context
+    /// The result is a base64 encoded PNG image
+    ///
+    /// # Arguments
+    /// * `ctx` - The context to use
+    ///
+    /// # Errors
+    /// * `AuthError::QrCodeError` - An error occurred while generating the QR code
     #[inline]
     fn get_totp_qr_code(&self, ctx: &Self::Context) -> Result<String, AuthError<Self>> {
         self.totp_from_ctx(ctx)?
@@ -422,6 +648,18 @@ pub trait Backend {
             .map_err(AuthError::QrCodeError)
     }
 
+    /// Start an OAuth2 session
+    /// This will return an `OAuthSession` that can be used to start the OAuth2 flow
+    /// The `auth_url` field should be used to redirect the user to the OAuth2 provider
+    /// The remainder of this session should be stored in the backend and used to complete the
+    /// authentication when the user is redirected back
+    ///
+    /// # Arguments
+    /// * `ctx` - The context to use
+    /// * `scopes` - The scopes to request
+    ///
+    /// # Errors
+    /// * `AuthError::UrlParseError` - An error occurred while parsing the OAuth2 URLs
     fn start_oauth_session<O>(
         &self,
         _ctx: &Self::Context,
@@ -454,6 +692,20 @@ pub trait Backend {
         })
     }
 
+    /// Finish an OAuth2 session
+    /// This will complete the OAuth2 flow and return an `OAuthResult` with the authentication
+    /// token and OAuth2 tokens
+    ///
+    /// # Arguments
+    /// * `ctx` - The context to use
+    /// * `oauth_session` - The OAuth2 session to finish
+    /// * `code` - The OAuth2 code to use
+    /// * `state` - The OAuth2 state to use
+    ///
+    /// # Errors
+    /// * `AuthError::InvalidOAuthState` - The OAuth2 state is invalid
+    /// * `AuthError::OAuthTokenError` - An error occurred while exchanging the OAuth2 code
+    /// * `AuthError::BackendError` - A backend error occurred while generating the token
     async fn finish_oauth_session(
         &self,
         ctx: &Self::Context,
@@ -491,10 +743,16 @@ pub trait Backend {
     // TODO: SAML support
 }
 
+/// Implement this trait for each OAuth2 Provider and provide the required configuration constants
 pub trait OAuthProvider {
+    /// The OAuth2 client ID
     const CLIENT_ID: &'static str;
+    /// The OAuth2 client secret
     const CLIENT_SECRET: &'static str;
+    /// The OAuth2 authorization URL
     const AUTH_URL: &'static str;
+    /// The OAuth2 token URL
     const TOKEN_URL: &'static str;
+    /// The OAuth2 redirect URL
     const REDIRECT_URL: &'static str;
 }
